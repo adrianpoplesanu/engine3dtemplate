d9f857411d7e490d1ac5156c3b744848800d3f55
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-10-01 14:53:11 +0300

    .gitignore + documentation links

--- Engine3DTemplate/src/main/java/ver01/MainApplication.java
+++ Engine3DTemplate/src/main/java/ver01/MainApplication.java
@@ -2,6 +2,8 @@ package ver01;
 
 public class MainApplication {
     public static void main(String[] args) {
+        // https://lwjglgamedev.gitbooks.io/3d-game-development-with-lwjgl/content/
+        // https://github.com/lwjglgamedev/lwjglbook
         GameLoop gameLoop = new GameLoop();
         gameLoop.start();
     }
